8e099d33d93a122f6af91cc749ab10b508876861
Joshua Lawson <joshua7lawson@gmail.com>
2024-04-16 16:06:28 +0100

    added pokemon reqs

--- apis/poke_req.py
+++ apis/poke_req.py
@@ -1,11 +1,43 @@
 import requests
 import json
+import random
 
-pokemon_req = requests.get("https://pokeapi.co/api/v2/pokemon/dragonite")
+def pokemon_generator():
+    id = random.randint(1,101)
+    return id
 
-# data_dict = pokemon_req.json()
-# print(data_dict)
+player_one = input("Enter pokemon ID (number 1-100)")
+player_two = input("Enter pokemon ID (number 1-100)")
 
-poke_data = json.loads(pokemon_req.text)
+# poke_req = requests.get("https://pokeapi.co/api/v2/pokemon/" + str(pokemon_generator()))
+# print(poke_req.json())
+poke_req = requests.get("https://pokeapi.co/api/v2/pokemon/" + str(player_one))
+
+poke_req2 = requests.get("https://pokeapi.co/api/v2/pokemon/" + str(player_two))
+
+# poke_req2 = requests.get("https://pokeapi.co/api/v2/pokemon/" + str(pokemon_generator()))
+# print(poke_req2.json())
+
+poke_data = json.loads(poke_req.text)
+poke_data2 = json.loads(poke_req2.text)
 # print(type(poke_data))
-print(poke_data["name"])
+
+# user_id = input("Enter pokemon ID (number 1-100)")
+
+# two random computer inputs
+print(f"Player one : {poke_data['name']}")
+print(f"Player two : {poke_data2['name']}")
+
+
+
+def pokemon_compare(player_one, player_two):
+    print(f"This is Player 1 height: {float(player_one['height']) * 10} cm")
+    print(f"This is Player 2 height: {float(player_two['height']) * 10} cm")
+    if player_one["height"] > player_two["height"]:
+        return "Player one has won!"
+    elif player_one["height"] == player_two["height"]:
+        return "Its a draw!"
+    else:
+        return "Player two has won!"
+
+print(pokemon_compare(poke_data, poke_data2))
